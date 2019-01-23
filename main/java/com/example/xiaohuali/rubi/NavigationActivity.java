f253583b5a15cae1c5f0b4336b2a9791c32e7485
2020czhang <37252947+2020czhang@users.noreply.github.com>
2019-01-23 00:56:19 -0500

    Add files via upload

--- main/java/com/example/xiaohuali/rubi/NavigationActivity.java
+++ main/java/com/example/xiaohuali/rubi/NavigationActivity.java
@@ -1,13 +1,32 @@
 package com.example.xiaohuali.rubi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NavigationActivity extends AppCompatActivity {
+
+    private Button mScrambled;
+    private Button mOne;
+    private Button mTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+
+        mScrambled = (Button)findViewById(R.id.scrambled);
+        mOne = (Button)findViewById(R.id.layer_one);
+        mTwo = (Button)findViewById(R.id.layer_two);
+
+        mOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(NavigationActivity.this, One.class);
+                startActivity(i);
+            }
+        });
     }
 }
